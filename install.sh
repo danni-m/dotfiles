623dea299e30319821e96554a248bf1cadc8cb98
Danni Moiseyev <danni@rustybits.io>
2025-09-28 14:34:47 +0300

    WIP

--- install.sh
+++ install.sh
@@ -1,7 +1,34 @@
-#!/bin/bash
+#!/bin/zsh
 
 ./install_nvim.sh
+
+RAW_ARCH=$(uname -m)
+
+echo "Detected architecture: $ARCH"
+
+# Pick correct tar.gz package
+if [[ "$RAW_ARCH" == "x86_64" ]]; then
+    ARCH="x86_64"
+elif [[ "$RAW_ARCH" == "aarch64" ]]; then
+    ARCH="arm64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_${ARCH}.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+source ~/.zshrc
+nvm install node
+npm install -g tree-sitter-cli
 
 mkdir -p ~/.config
 cd ~/.config
 git clone https://github.com/danni-m/nvim.git
+
+sudo apt-get update && apt-get install -y fzf
+sudo apt install lazygit fd-find
